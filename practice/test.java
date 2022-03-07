78bea6ae2a809fe036a9d13cba90510c46ad024b
ritikach16 <100216272+ritikach16@users.noreply.github.com>
2022-03-07 09:09:20 +0530

    removing practice dir

--- practice/test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args){
-        System.out.println("***jdbsbdbcjdbjhb-4.Testing hdbcbdbjhb main() methods.***");
-
-    }
-    public static void main(String... args){
-        
-    }
-}
